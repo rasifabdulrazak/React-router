b07cfa3d86e72c5bf7c4237ddcb4b51442e239ae
Mohammed Rasif <rasifrazak123@gmail.com>
2022-03-02 10:07:38 +0530

    ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './Container/About';
+import Profile from './Container/Profile';
+import {useState} from 'react'
+import {BrowserRouter as Router,Routes,Route,Link,useNavigate} from 'react-router-dom'
 
 function App() {
+  const [state, setState] = useState('')
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>navigate.push('/about')}>About</button>
+      <button onClick={()=>navigate.push('/profile')}>Profile</button>
+    
+        <Route element={About} path='/about' /> 
+        <Route element={Profile} path='/profile' />
+ 
     </div>
   );
 }
